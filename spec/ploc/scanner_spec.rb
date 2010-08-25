c4990c26d5cc2c6d88c8ae472bea4a8014d18de6
Juan Manuel Barreneche <snipperme@gmail.com>
2010-08-25 17:05:56 -0300

    Added test for tracking current line and line number

--- spec/ploc/scanner_spec.rb
+++ spec/ploc/scanner_spec.rb
@@ -10,7 +10,7 @@ describe Ploc::Scanner do
     array.map(&:to_s).first.should == '.'
   end
   it 'should tokenize text and symbols' do
-    scanner = Ploc::Scanner.new(StringIO.new("symbol := 23a;\nl3tt3randnumb3r:=3*(symbol + symbol)"))
+    scanner = Ploc::Scanner.new(StringIO.new("    symbol := 23a;   \n\t  \t l3tt3randnumb3r:=3*(symbol + symbol)     "))
     scanner.to_a.map(&:to_s).should == %w[symbol := 23 a ; l3tt3randnumb3r := 3 * ( symbol + symbol )]
   end
   it 'should tokenize iteratibely' do
@@ -23,5 +23,22 @@ describe Ploc::Scanner do
     input.should_receive(:gets).and_return('anything black')
     scanner.next.should == 'anything'
     scanner.next.should == 'black'
+  end
+  it 'should keep track of current line' do
+    input = mock('input')
+    input.should_receive(:gets).and_return('something blue')
+    input.should_receive(:gets).and_return('anything black')
+    input.stub(:eof?).and_return(false)
+    scanner = Ploc::Scanner.new(input)
+    scanner.next
+    scanner.current_line.should == 'something blue'
+    scanner.line_number == 1
+    scanner.next
+    scanner.current_line.should == 'something blue'
+    scanner.line_number == 1
+
+    scanner.next
+    scanner.current_line.should == 'anything black'
+    scanner.line_number == 2
   end
 end
