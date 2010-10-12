2a73e234320500479d8bf3f51470a7fe11385fb2
Juan Manuel Barreneche <snipperme@gmail.com>
2010-10-12 19:19:33 -0300

    Basic scoping and subscoping

--- lib/ploc/scope.rb
+++ lib/ploc/scope.rb
@@ -1,29 +1,46 @@
 module Ploc
   class DuplicateDeclarationError < StandardError; end
   class Scope
-    def initialize
+    def initialize(parent = nil)
+      @parent = parent
       @local_declarations_hash = {constants:[], variables: [], procedures: []}
     end
     def constants
-      []
+      local_constants + parent_constants
     end
     def variables
       local_variables + parent_variables
     end
     def procedures
-      []
+      local_procedures + parent_procedures
     end
     def parent
       @parent
     end
     def declare(type, value)
-      declare_variable(value)
+      raise DuplicateDeclarationError if local_declarations.include? value
+      case type
+      when :variable
+        declare_variable(value)
+      when :constant
+        declare_constant(value)
+      else
+        declare_procedure(value)
+      end
       value
+    end
+    def build_subcontext
+      Scope.new(self)
     end
   private
     def declare_variable(value)
-      raise DuplicateDeclarationError if local_declarations.include? value
       local_variables << value
+    end
+    def declare_constant(value)
+      local_constants << value
+    end
+    def declare_procedure(value)
+      local_procedures << value
     end
     def local_variables
       local_declarations_hash[:variables]
@@ -36,6 +53,12 @@ module Ploc
     end
     def parent_variables
       @parent ? @parent.variables : []
+    end
+    def parent_constants
+      @parent ? @parent.constants : []
+    end
+    def parent_procedures
+      @parent ? @parent.procedures : []
     end
     def local_declarations
       local_variables + local_procedures + local_constants
--- spec/ploc/scope_spec.rb
+++ spec/ploc/scope_spec.rb
@@ -19,4 +19,48 @@ describe Ploc::Scope do
       }.should raise_error(Ploc::DuplicateDeclarationError)
     end
   end
+  context 'declaring constants' do
+    it 'should allow to declare variables' do
+      foo_cons = subject.declare(:constant, :foo)
+      subject.should have(1).constants
+      subject.constants.should include(foo_cons)
+    end
+    it 'should\'nt allow to declare already declared variables' do
+      foo_cons = subject.declare(:constant, :foo)
+      lambda {
+        subject.declare(:constant, :foo)
+      }.should raise_error(Ploc::DuplicateDeclarationError)
+    end
+  end
+  context 'declaring procedures' do
+    it 'should allow to declare procedures' do
+      foo_proc = subject.declare(:procedure, :foo)
+      subject.should have(1).procedures
+      subject.procedures.should include(foo_proc)
+    end
+    it 'should\'nt allow to declare already declared procedures' do
+      foo_proc = subject.declare(:procedure, :foo)
+      lambda {
+        subject.declare(:procedure, :foo)
+      }.should raise_error(Ploc::DuplicateDeclarationError)
+    end
+  end
+  context 'nesting scopes' do
+    before(:each) do
+      @parent = subject
+      @subcontext = @parent.build_subcontext
+    end
+    it 'should allow to create subcontexts' do
+      @subcontext.parent.should == @parent
+    end
+    it 'subcontext should have parents declarations' do
+      foo_bar = @parent.declare(:variable, :foo)
+      @subcontext.variables.should include(foo_bar)
+    end
+    it 'subcontext should allow shadowing declarations' do
+      foo_bar = @parent.declare(:variable, :foo)
+      foo_bar_shadow = @subcontext.declare(:variable, :foo)
+      @subcontext.variables.should include(foo_bar)
+    end
+  end
 end
