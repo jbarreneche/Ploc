009836a0327af2e57fde446d1b29b34008ccd5fc
Juan Manuel Barreneche <snipperme@gmail.com>
2010-10-16 20:50:00 -0300

    minor refactor in BinaryData

--- lib/ploc/binary_data.rb
+++ lib/ploc/binary_data.rb
@@ -1,10 +1,9 @@
 module Ploc
   class BinaryData
+    attr_reader :data
+    alias :to_s :data
     def initialize(string)
       @data = string.gsub(/#.*$/,'').split.map {|s| s.to_i.chr }.join
-    end
-    def to_s
-      @data
     end
   end
 end
