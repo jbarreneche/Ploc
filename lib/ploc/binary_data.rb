72334f76d1cc5c3814c65913289c3bbb57eea284
Juan Manuel Barreneche <snipperme@gmail.com>
2010-10-22 10:13:58 -0300

    Fixing Binary data to output hexa instead of decimal - Adding binary data generation from integers

--- lib/ploc/binary_data.rb
+++ lib/ploc/binary_data.rb
@@ -2,8 +2,13 @@ module Ploc
   class BinaryData
     attr_reader :data
     alias :to_s :data
+    INT_CONVERSION = 2 ** 32
     def initialize(string)
-      @data = string.gsub(/#.*$/,'').split.map {|s| s.to_i.chr }.join
+      @data = string.gsub(/#.*$/,'').split.map {|s| s.to_i(16).chr }.join
+    end
+    def self.parse_int(int)
+      int = int < 0 ? INT_CONVERSION + int : int
+      new int.to_s(16).rjust(8, '0').scan(/../).reverse.join(' ')
     end
   end
 end
--- spec/ploc/binary_data_spec.rb
+++ spec/ploc/binary_data_spec.rb
@@ -5,20 +5,26 @@ require 'stringio'
 describe Ploc::BinaryData do
   it 'should stream correctly one simply pair' do
     stream = StringIO.new
-    stream << Ploc::BinaryData.new("#{?a.ord}") # ?a.ord => 97
+    stream << Ploc::BinaryData.new("#{?a.ord.to_s(16)}") # ?a.ord => 97
     stream.string.should == "a"
   end
   it 'should stream correctly multiple pairs' do
     stream = StringIO.new
-    stream << Ploc::BinaryData.new("#{?a.ord} #{?b.ord}") # ?a.ord => 97
+    stream << Ploc::BinaryData.new("#{?a.ord.to_s(16)} #{?b.ord.to_s(16)}") # ?a.ord => 97
     stream.string.should == "ab"
   end
   it 'should discard comments' do
     stream = StringIO.new
     stream << Ploc::BinaryData.new(<<-BIN_DATA)
-      #{?a.ord} #{?b.ord} # This are the first 2 letters
-      #{?c.ord} #{?d.ord} # This are the next 2 letters
+      #{?a.ord.to_s(16)} #{?b.ord.to_s(16)} # This are the first 2 letters
+      #{?c.ord.to_s(16)} #{?d.ord.to_s(16)} # This are the next 2 letters
     BIN_DATA
     stream.string.should == "abcd"
+  end
+  it 'should parse int into little? endian format' do
+    Ploc::BinaryData.parse_int(3).to_s.should == "\x3\0\0\0"
+  end
+  it 'should parse negatives int into little? endian format' do
+    Ploc::BinaryData.parse_int(-1).to_s.should == "\xff\xff\xff\xff"
   end
 end
