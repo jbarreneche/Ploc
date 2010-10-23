5d5d3ccc6cff3d518d122d5e6a4384e1146bb562
Juan Manuel Barreneche <snipperme@gmail.com>
2010-10-23 12:50:24 -0300

    Added some semantic_rules specs (all pending... only to avoid forgetting them!)

--- spec/ploc/pl0/semantic_rules_spec.rb
+++ spec/ploc/pl0/semantic_rules_spec.rb
@@ -4,12 +4,51 @@ require 'ploc/pl0/language'
 
 module Ploc::PL0
   describe SemanticRules do
-    it 'should notify context of the begining and ending of the program' do
-      context = CompilingContext.new
-      Language.context_builder.stub(:call) {|source_code| context.source_code=(source_code); source_code.context=(context) }
-      context.should_receive :initialize_new_program!
-      context.should_receive :complete_program
+    before(:each) do
+      @context = CompilingContext.new
+      Language.context_builder.stub(:call) {|source_code| @context.source_code=(source_code); source_code.context=(@context) }
+    end
+    it 'should notify context of the begining' do
+      @context.should_receive :initialize_new_program!
       Language.compile StringIO.new(".")
+    end
+    it 'should notify context of the end after the begining' do
+      @context.should_receive(:initialize_new_program!).ordered
+      @context.should_receive(:complete_program).ordered
+      Language.compile StringIO.new(".")
+    end
+    describe "Declaring constants" do
+      it 'should allow to declare one constant' do
+        pending
+        @context.should_receive(:declare_constant).with("V", 3)
+        Language.compile StringIO.new("CONST V = 3.")
+      end
+      it 'should allow to declare multiple constants' do
+        pending
+        @context.should_receive(:declare_constant).with("V", 3).ordered
+        @context.should_receive(:declare_constant).with("W", 5).ordered
+        Language.compile StringIO.new("CONST V = 3, W = 5.")
+      end
+    end
+    describe "Declaring variables" do
+      it 'should allow to declare one variable' do
+        pending
+        @context.should_receive(:declare_variable).with("V")
+        Language.compile StringIO.new("VAR V.")
+      end
+      it 'should allow to declare multiple variables' do
+        pending
+        @context.should_receive(:declare_variable).with("V").ordered
+        @context.should_receive(:declare_variable).with("W").ordered
+        Language.compile StringIO.new("CONST V, W.")
+      end
+    end
+    describe "Declaring procedures" do
+      it 'should allow to declare one procedure' do
+        pending
+        @context.should_receive(:declare_procedure).with("P")
+        Language.compile StringIO.new("PROCEDURE P;;.")
+      end
     end
   end
 end
