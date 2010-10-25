512df108387f6ca657ccfea10d594c4d4d77b1ff
Juan Manuel Barreneche <snipperme@gmail.com>
2010-10-25 20:48:43 -0300

    DRY - Address and BinaryData

--- lib/ploc/address.rb
+++ lib/ploc/address.rb
@@ -6,20 +6,22 @@ module Ploc
     def initialize(value)
       @value = value
     end
-    def -(address)
-      other_value = self.class === address ? address.value : address
-      self.class.new(value - other_value)
+    def -(address_or_value)
+      self.class.new(value - extract_value(address_or_value))
     end
-    def +(address)
-      other_value = self.class === address ? address.value : address
-      self.class.new(value + other_value)
+    def +(address_or_value)
+      self.class.new(value + extract_value(address_or_value))
     end
     def to_bin
-      @bin_value ||= BinaryData.new(self.value).to_s
+      @bin_value ||= BinaryData.new(self.value)
     end
     def ==(o)
       return self.value == o.value if self.class === o
       super
+    end
+  private
+    def extract_value(address_or_value)
+      self.class === address_or_value ? address_or_value.value : address_or_value
     end
   end
 end
--- lib/ploc/binary_data.rb
+++ lib/ploc/binary_data.rb
@@ -1,8 +1,23 @@
 module Ploc
   class BinaryData
+    INT_CONVERSION = 2 ** 32
+    class << self
+      def parse_string(string)
+        string.gsub(/#.*$/,'').split.map {|s| s.to_i(16).chr }.join
+      end
+      def parse_int(int)
+        data = ""
+        4.times do 
+          int, mod = int.divmod(256)
+          data << mod.chr
+        end
+        data
+      end
+    end
+
     attr_reader :data
     alias :to_s :data
-    INT_CONVERSION = 2 ** 32
+    alias :to_str :to_s
     def initialize(*values)
       @data = values.map do |value|
         case value
@@ -15,16 +30,9 @@ module Ploc
         end
       end.join
     end
-    def self.parse_string(string)
-      string.gsub(/#.*$/,'').split.map {|s| s.to_i(16).chr }.join
-    end
-    def self.parse_int(int)
-      data = ""
-      4.times do 
-        int, mod = int.divmod(256)
-        data << mod.chr
-      end
-      data
+    def ==(other)
+      return self.to_str == other.to_str if other.respond_to? :to_str
+      super
     end
   end
 end
