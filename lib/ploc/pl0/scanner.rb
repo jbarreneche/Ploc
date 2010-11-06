d966d5ad84e3d778493b97fba23fe9b8f79f5b7a
Juan Manuel Barreneche <snipperme@gmail.com>
2010-11-06 19:02:55 -0300

    Fixed parsing tokenization of multiple char operators

--- lib/ploc/pl0/scanner.rb
+++ lib/ploc/pl0/scanner.rb
@@ -3,7 +3,7 @@ require 'ploc/scanner'
 module Ploc::PL0
   class Scanner < Ploc::Scanner
     string_regex = ->(sep) {"#{sep}[^#{sep}]*#{sep}|#{sep}[^#{sep}]*$"}
-    REGEX = /#{string_regex["'"]}|#{string_regex['"']}|\d+|\w+|:=|\S/
+    REGEX = /#{string_regex["'"]}|#{string_regex['"']}|\d+|\w+|:=|<=|<>|>=|\S/
     def initialize(input)
       super(input, REGEX)
     end
