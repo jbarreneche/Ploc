be10354544485d769092e5ae763862e4dd5120f9
Juan Manuel Barreneche <snipperme@gmail.com>
2010-11-07 16:01:09 -0300

    Allowing terminal syntax to be extended

--- lib/ploc/language_node/terminal.rb
+++ lib/ploc/language_node/terminal.rb
@@ -2,9 +2,10 @@ require 'ploc/token_matcher'
 
 module Ploc::LanguageNode
   class Terminal < Base
-    attr_reader :matcher
+    attr_reader :matcher, :language_extensions
     def initialize(language, matcher, *symbols, &block)
       @matcher = ::Ploc::TokenMatcher.new(matcher, *symbols)
+      @language_extensions = []
       super(language, &block)
     end
     def call_without_callbacks(source_code)
@@ -14,7 +15,7 @@ module Ploc::LanguageNode
         current
       else
         report_found_unexpected_token(source_code, "Expecting #{self.matcher.inspect}")
-        nil
+        source_code.next_token if language_extensions.any? {|le| le.matches?(source_code.current_token) }
       end
     end
     def matches_first?(token)
@@ -22,6 +23,9 @@ module Ploc::LanguageNode
     end
     def inspect
       "<Node terminal:#{self.matcher.inspect}>"
+    end
+    def extend_to(matcher, *symbols)
+      @language_extensions << ::Ploc::TokenMatcher.new(matcher, *symbols)
     end
   end
 end
---
+++ spec/ploc/language_node/terminal_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+require 'ploc/language_node'
+
+module Ploc::LanguageNode
+  describe Terminal do
+    let(:language) { stub('Language') }
+    let(:errors) { stub('Errors').as_null_object }
+    subject { Terminal.new(language, Fixnum, "1")}
+    describe 'Transversing' do
+      it 'advances source_code token' do
+        source_code = mock('SourceCode', current_token: 1)
+        source_code.should_receive(:next_token)
+        subject.call source_code
+      end
+    end
+    describe 'language extensions' do
+      before(:each) do
+        subject.extend_to Fixnum, "2"
+      end
+      it 'skips token if current matches the extension' do
+        source_code = mock('SourceCode', current_token: 2, errors: errors)
+        source_code.should_receive(:next_token)
+        subject.call source_code
+      end
+      it 'reports the error to source_code' do
+        source_code = mock('SourceCode', current_token: 2, next_token: 3, errors: errors)
+        errors.should_receive(:<<)
+        subject.call source_code
+      end
+      it 'doesnt skip token if current doesnt match any extension' do
+        source_code = mock('SourceCode', current_token: 3, errors: errors)
+        source_code.should_not_receive(:next_token)
+        subject.call source_code
+      end
+    end
+  end
+end
